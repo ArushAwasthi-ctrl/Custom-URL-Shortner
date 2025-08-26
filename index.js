0496fcd0621b0ba2f9f20a5831b55a8e6d1895e8
Arush <arushawasthi3@gmail.com>
2025-08-26 11:44:13 +0530

    understanding views and ejs and server side rendering

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const express = require('express');
-const { log } = require('node:console');
-const dotenv = require('dotenv');
+const express = require("express");
+const { log } = require("node:console");
+const path = require('path');
+const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
-const urlRouter = require('./routes/url.js')
-const { dbCall} = require('./db/dbCall.js')
-const path = require('path');
+const urlRouter = require("./routes/url.js");
+const { dbCall } = require("./db/dbCall.js");
 // connect with data base
-    dbCall();
+dbCall();
 
 // middleware to accept form input and JSON
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// whenever we request sever with /url route the urlRouter will fire 
-app.set("view engine","ejs");
-app.set('views',path.resolve('./views'))
-app.use('/url/allUrls',(req,res)=>{
-    app.render('homepage');
-})
-app.use('/url',urlRouter);
 
-app.listen(process.env.PORT , ()=>{
-    console.log(`Server is Listening on Port: ${process.env.PORT}`);
-})
+// using views to perform server side rendering using ejs 
+    app.set("view engine","ejs");
+    app.set("views",path.resolve('./views'));
+    app.use("/homepage",(req,res)=>{
+           res.render('home-page');
+    })
+ 
+
+
+// whenever we request sever with /url route the urlRouter will fire
+app.use("/url", urlRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is Listening on Port: ${process.env.PORT}`);
+});
